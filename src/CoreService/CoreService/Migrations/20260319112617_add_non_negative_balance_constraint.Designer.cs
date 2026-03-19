583658a5d86ac77572d600f136fad47994cffb61
Maxim Shukevich <182817650+mxshkvch@users.noreply.github.com>
2026-03-19 18:28:20 +0700

    Add migrations

--- src/CoreService/CoreService/Migrations/20260319112617_add_non_negative_balance_constraint.Designer.cs
+++ src/CoreService/CoreService/Migrations/20260319112617_add_non_negative_balance_constraint.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using CoreService.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 
@@ -11,9 +12,11 @@ using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 namespace CoreService.Migrations
 {
     [DbContext(typeof(AppDbContext))]
-    partial class AppDbContextModelSnapshot : ModelSnapshot
+    [Migration("20260319112617_add_non_negative_balance_constraint")]
+    partial class add_non_negative_balance_constraint
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ src/CoreService/CoreService/Migrations/20260319112617_add_non_negative_balance_constraint.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace CoreService.Migrations
+{
+    /// <inheritdoc />
+    public partial class add_non_negative_balance_constraint : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddCheckConstraint(
+                name: "CK_Accounts_Balance_NonNegative",
+                table: "Accounts",
+                sql: "\"Balance\" >= 0");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropCheckConstraint(
+                name: "CK_Accounts_Balance_NonNegative",
+                table: "Accounts");
+        }
+    }
+}
