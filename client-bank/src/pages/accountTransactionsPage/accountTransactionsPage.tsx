16e1c87af83999762064846524b624169331aab3
ruvich <alaptev1308@gmail.com>
2026-03-22 23:20:12 +0700

    веб сокеты

--- client-bank/src/pages/accountTransactionsPage/accountTransactionsPage.tsx
+++ client-bank/src/pages/accountTransactionsPage/accountTransactionsPage.tsx
@@ -5,8 +5,9 @@ import { useTheme } from "../../shared/lib/provider/themeProvider";
 import { fetchTransactionsForAccount } from "../../features/accounts/useCases/fetchTransactions";
 import type { Transaction } from "../../shared/lib/api/transactionsHistory";
 import { TransactionTable } from "../../entities/transactionTable";
-import { PaginationComponent } from "../../shared/ui/components/pagination"
+import { PaginationComponent } from "../../shared/ui/components/pagination";
 import { SpinnerComponent } from "../../shared/ui/components/spinner";
+import { createConnection } from "../../shared/lib/ws/signalR";
 
 export const AccountTransactionsPage = () => {
   const { accountId } = useParams<{ accountId: string }>();
@@ -18,16 +19,60 @@ export const AccountTransactionsPage = () => {
 
   const { theme } = useTheme();
 
-  useEffect(() => {
-    if (!accountId) return;
+  const refreshTransactions = () => {
     setLoading(true);
-    fetchTransactionsForAccount(accountId, currentPage, pageSize)
+    fetchTransactionsForAccount(accountId!, currentPage, pageSize)
       .then(({ transactions, totalPages }) => {
         setTransactions(transactions);
         setTotalPages(totalPages);
       })
       .finally(() => setLoading(false));
+  };
+
+  useEffect(() => {
+    if (!accountId) return;
+    refreshTransactions();
   }, [accountId, currentPage]);
+
+  useEffect(() => {
+  if (!accountId) return;
+
+  const token = localStorage.getItem("accessToken");
+  if (!token) return;
+
+  const connection = createConnection(token);
+  let timeout: ReturnType<typeof setTimeout>;
+
+  connection.start().then(() => {
+    console.log("WS connected");
+
+    connection.on("operationUpdated", (message) => {
+      console.log("WS event:", message);
+
+      if (
+        message.type === "operation_invalidation" &&
+        (message.accountId === accountId || message.targetAccountId === accountId)
+      ) {
+        clearTimeout(timeout);
+        timeout = setTimeout(() => {
+          setLoading(true);
+          fetchTransactionsForAccount(accountId, currentPage, pageSize)
+            .then(({ transactions, totalPages }) => {
+              setTransactions(transactions);
+              setTotalPages(totalPages);
+            })
+            .finally(() => setLoading(false));
+        }, 300);
+      }
+    });
+  }).catch(err => {
+    console.error("Failed to start WS connection:", err);
+  });
+
+  return () => {
+    connection.stop();
+  };
+}, [accountId]);
 
   return (
     <Container className="py-5">
